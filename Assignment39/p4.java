8ffceaaf802a53ab345667bb380cc1e464d8859f
KshitijJaiswal747 <jaiswalkshitij143@gmail.com>
2026-07-29 01:09:02 +0530

    Add files via upload

---
+++ Assignment39/p4.java
@@ -0,0 +1,19 @@
+class p4
+{
+    static char ch = 'A';
+
+    public static void Display()
+    {
+        if(ch <= 'F')
+        {
+            System.out.print(ch + " ");
+            ch++;
+            Display();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Display();
+    }
+}
